8c2f36948d8282d429fd62dc57eaac3bfb483c9d
Maxim Kiryakov <71668634+maxbritva@users.noreply.github.com>
2024-09-06 15:51:38 +0300

    some new

---
+++ Assets/Scripts/Game/GameRoot/UIRootView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.GameRoot
+{
+    public class UIRootView : MonoBehaviour
+    {
+        [SerializeField] private GameObject _loadingScreen;
+
+        private void Awake() => HideLoadingScreen();
+
+        public void ShowLoadingScreen() => _loadingScreen.SetActive(true);
+        public void HideLoadingScreen() => _loadingScreen.SetActive(false);
+    }
+}
